da967a36ca6e88cbf09fa332d4846f3fd4e9a1cd
Oberdan <oberdan.bitencourt@gmail.com>
2014-01-13 17:56:11 -0200

    table ordering by progress

--- app/js/controllers/dashboardController.js
+++ app/js/controllers/dashboardController.js
@@ -3,18 +3,28 @@
 checklistApp.controller('dashboardController', 
         function dashboardController($scope){
 
+          $scope.sortOrder = 'progress'
           $scope.newEmployees = [
             {
+              progress: 66,
+              name: 'Priscila Koeck Machado',
+              date: '14/02/2014',
+              responsible: 'peoplecare@bravisoftware.com'            
+            },
+            {
+              progress: 10,
               name: 'Oberdan Bitencourt Ferreira',
               date: '12/01/2014',
               responsible: 'peoplecare@bravisoftware.com'
             },
             {
+              progress: 20,
               name: 'Antonio José Resende de Campos',
               date: '12/01/2014',
               responsible: 'peoplecare@bravisoftware.com'            
             },
             {
+              progress: 30,
               name: 'Ruann Magalhães Homem',
               date: '12/01/2014',
               responsible: 'peoplecare@bravisoftware.com'            
@@ -23,11 +33,13 @@ checklistApp.controller('dashboardController',
 
         $scope.readyEmployees = [
           {
+            progress: 100,
             name: 'Max Claus Nunes',
             date: '01/12/2013',
             responsible: 'peoplecare@bravisoftware.com'
           },
           {
+            progress: 100,
             name: 'Rodrigo Bastos Vieira',
             date: '10/07/1931',
             responsible: 'peoplecare@bravisoftware.com'            
