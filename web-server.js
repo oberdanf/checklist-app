32f6d28c253f6757f06d56770b784742b74dfad9
Oberdan Bitencourt Ferreira <oberdan.bitencourt@gmail.com>
2014-01-27 01:13:14 -0200

    Adding index.html and the renewed web-server.js that were missing by the last commit.

---
+++ web-server.js
@@ -0,0 +1,56 @@
+'use strict';
+
+// Module dependencies.
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs');
+
+var app = express();
+
+// Connect to database
+// var db = require('./lib/db/mongo');
+
+// Bootstrap models
+// var modelsPath = path.join(__dirname, 'lib/models');
+// fs.readdirSync(modelsPath).forEach(function (file) {
+//   if(!RegExp('^.+?\.sw.?$', 'i').test(file)) {
+//     require(modelsPath + '/' + file);
+//   }
+// });
+
+// Populate empty DB with uummy data
+// require('./lib/db/dummydata');
+
+// Controllers
+// var api = require('./lib/controllers/api');
+
+// Express Configuration
+app.configure(function(){
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'app')));
+});
+
+// app.configure('development', function(){
+//   app.use(express.static(path.join(__dirname, '.tmp')));
+//   app.use(express.static(path.join(__dirname, 'app')));
+//   app.use(express.errorHandler());
+// });
+
+// app.configure('production', function(){
+//   app.use(express.favicon(path.join(__dirname, 'public/favicon.ico')));
+//   app.use(express.static(path.join(__dirname, 'public')));
+// });
+
+// Routes
+// app.get('/api/verbs/:book', api.verbs);
+// app.get('/api/houses', api.houses);
+// app.get('/api/general-result', api.generalResult);
+
+// Start server
+var port = process.env.PORT || 8888;
+app.listen(port, function () {
+  console.log('Express server listening on port %d in %s mode', port, app.get('env'));
+});
